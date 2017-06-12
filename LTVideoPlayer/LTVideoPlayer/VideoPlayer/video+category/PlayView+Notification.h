05540cd3e5bbcb6c39aeb0bc29acbd63ddddce1e
孟令通 <ak-7755@qq.com>
2017-06-12 18:25:41 +0800

    增加网络请求层HttpTool 增加通知中心类目

---
+++ LTVideoPlayer/LTVideoPlayer/VideoPlayer/video+category/PlayView+Notification.h
@@ -0,0 +1,13 @@
+//
+//  PlayView+Notification.h
+//  LTVideoPlayer
+//
+//  Created by 孟令通 on 2017/6/12.
+//  Copyright © 2017年 LryMlt. All rights reserved.
+//
+
+#import "PlayView.h"
+
+@interface PlayView (Notification)
+
+@end
---
+++ LTVideoPlayer/LTVideoPlayer/VideoPlayer/video+category/PlayView+Notification.m
@@ -0,0 +1,13 @@
+//
+//  PlayView+Notification.m
+//  LTVideoPlayer
+//
+//  Created by 孟令通 on 2017/6/12.
+//  Copyright © 2017年 LryMlt. All rights reserved.
+//
+
+#import "PlayView+Notification.h"
+
+@implementation PlayView (Notification)
+
+@end
